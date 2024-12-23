19069f446f47636e89ab1b42530da92150663f7d
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-12-23 09:25:18 -0500

    init: areal stats load data

--- scripts/03_areal_analysis.R
+++ scripts/03_areal_analysis.R
@@ -1,1 +1,18 @@
 library(spdep)
+library(sf)
+
+# Source util functions
+# - normalize_rates(dataset)
+source("R/utils.R")
+
+clean_data_dir <- file.path(getwd(), "data", "clean")
+output_dir <- file.path(getwd(), "out", "areal")
+
+cached_covid_v_er_2020_src <- file.path(clean_data_dir, "covid_nyer_merged_geo_2020.gpkg")
+cached_covid_v_er_2024_src <- file.path(clean_data_dir, "covid_nyer_merged_geo_2024.gpkg")
+
+covid_v_er_2020_raw <- st_read(cached_covid_v_er_2020_src)
+covid_v_er_2024_raw <- st_read(cached_covid_v_er_2024_src)
+
+covid_v_er_2020 <- normalize_rates(covid_v_er_2020_raw)
+covid_v_er_2024 <- normalize_rates(covid_v_er_2024_raw)
