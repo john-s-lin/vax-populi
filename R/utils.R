343655038a323a0856045adc0df9afc28f261a0f
john-s-lin <66440371+john-s-lin@users.noreply.github.com>
2024-12-24 15:33:53 -0500

    patch: update cleaning func

--- R/utils.R
+++ R/utils.R
@@ -2,28 +2,37 @@ library(dplyr)
 
 normalize_rates <- function(dataset) {
   # Convert case rates from rate/100_000 people to fraction between [0, 1]
-  dataset <- mutate(dataset, across(
-    c(
-      "COVID_CONFIRMED_CASE_RATE",
-      "COVID_CASE_RATE",
-      "COVID_DEATH_RATE"
-    ),
-    ~ . / 100000
-  ))
-  # Convert percentage vaccinated into fraction between [0, 1]
-  dataset <- mutate(dataset, across(
-    c(
-      "PERC_PARTIALLY",
-      "PERC_FULLY",
-      "PERC_1PLUS",
-      "PERC_ADDITIONAL",
-      "PERC_BIVALENT_ADDITIONAL"
-    ),
-    ~ . / 100
-  ))
-  # Store ratio of republican voters over sum of Dem + Rep parties, ignore Green and Libertarian
-  dataset$two_party_total <- dataset$republican + dataset$democratic
-  dataset$republican_two_party_frac <- dataset$republican / dataset$two_party_total
-  dataset$democrat_two_party_frac <- 1 - dataset$republican_two_party_frac
+  dataset <- dataset %>%
+    mutate(across(
+      c(
+        "COVID_CONFIRMED_CASE_RATE",
+        "COVID_CASE_RATE",
+        "COVID_DEATH_RATE"
+      ),
+      ~ . / 100000
+    )) %>%
+    # Convert percentage vaccinated into fraction between [0, 1]
+    mutate(across(
+      c(
+        "PERC_PARTIALLY",
+        "PERC_FULLY",
+        "PERC_1PLUS",
+        "PERC_ADDITIONAL",
+        "PERC_BIVALENT_ADDITIONAL"
+      ),
+      ~ . / 100
+    )) %>%
+    # Calculate fractions for two-party voting
+    mutate(
+      two_party_total = republican + democratic,
+      republican_two_party_frac = republican / two_party_total,
+      democrat_two_party_frac = 1 - republican_two_party_frac
+    ) %>%
+    # Replace NaN values with 0
+    mutate(across(
+      c(republican_two_party_frac, democrat_two_party_frac),
+      ~ if_else(is.nan(.), 0, .)
+    ))
+  
   return(dataset)
 }
